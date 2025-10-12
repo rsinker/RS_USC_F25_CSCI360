4ac82b70a5cc8632a0dc2fdba087ad7bb5c00e72
Roosevelt Sinker <66215548+rsinker@users.noreply.github.com>
2025-10-11 21:40:58 -0700

    lab 2 done

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -3,6 +3,7 @@ from collections import deque
 from heapq import heappush, heappop
 
 import numpy as np
+import heapq
 
 
 class TextbookStack(object):
@@ -58,11 +59,62 @@ def apply_sequence(stack, sequence):
 
 def a_star_search(stack):
     flip_sequence = []
+    pq = []
+    counter = 0
+
+    heapq.heappush(pq, (calculate_heuristic(stack, flip_sequence), counter, (stack, flip_sequence)))
+    counter += 1
 
     # --- v ADD YOUR CODE HERE v --- #
 
+    explored_set = set()
+    
+    while len(pq) != 0:
+        priority, tiebreaker, (current_stack, current_sequence) = heapq.heappop(pq)
+        stack_key = (tuple(current_stack.order), tuple(current_stack.orientations))
+        if stack_key in explored_set:
+            continue
+        explored_set.add(stack_key)
+        
+        if current_stack.check_ordered() == True:
+            return current_sequence
+        
+        for flip_position in range(1, current_stack.num_books + 1):
+            new_stack = current_stack.copy()
+            new_stack.flip_stack(flip_position)
+            
+            new_sequence = current_sequence + [flip_position]
+
+            heapq.heappush(pq, (calculate_heuristic(new_stack, new_sequence), counter, (new_stack, new_sequence)))
+            counter += 1
+
     return flip_sequence
     # ---------------------------- #
+
+def calculate_heuristic(stack, sequence):
+    heuristic = len(sequence)
+
+    for i in range(1, len(stack.order)):
+        book1 = stack.order[i - 1]
+        book2 = stack.order[i]
+    
+        book1orientation = stack.orientations[i - 1]
+        book2orientation = stack.orientations[i]
+
+        if (abs(book1 - book2) != 1):
+            heuristic += 1
+            continue
+        if (book1orientation != book2orientation):
+            heuristic += 1
+            continue
+        if (book1 > book2 and book1orientation == 1 and book2orientation == 1):
+            heuristic += 1
+            continue
+        if (book1 + 1 == book2 and book1orientation == 0 and book2orientation == 0):
+            heuristic += 1
+            continue
+
+    return heuristic
 
 
 def weighted_a_star_search(stack, epsilon=None, N=1):
@@ -74,7 +126,6 @@ def weighted_a_star_search(stack, epsilon=None, N=1):
     return flip_sequence
 
     # ---------------------------- #
-
 
 if __name__ == "__main__":
     test = TextbookStack(initial_order=[3, 2, 1, 0], initial_orientations=[0, 0, 0, 0])
